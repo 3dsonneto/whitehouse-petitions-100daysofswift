ce7c985e5bd80441933180c2d1528d21c7bd69f8
Edson <edson_neto1996@hotmail.com>
2021-04-17 19:43:16 -0300

    Defined the structures that json will load into

---
+++ Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Edson Neto on 17/04/21.
+//
+
+import Foundation
+
+struct Petitions: Codable{
+    var result: [Petition]
+}
--- Project7/ViewController.swift
+++ Project7/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    var petitions = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Title goes here" //Muda o Title da cell(porque tem style subtitle com title e detail)
+        cell.detailTextLabel?.text = "Subtitle goes here" //muda o Detail da cell
+        return cell
+    }
+    
 }
 
